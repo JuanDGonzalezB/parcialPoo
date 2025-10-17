7bc0a2f0edcc3c34494394202f5dc199d45980f8
Juan David Gonzalez Bermudez <juandgonzalezb@udistrital.edu.co>
2025-10-16 22:45:09 -0500

    Se actualiza el main agregando la clase ValidadorPalindromo

--- src/parcialpoo/Main.java
+++ src/parcialpoo/Main.java
@@ -13,14 +13,17 @@ public class Main {
             System.out.println("1. Zodiaco");
             System.out.println("2. Validador de edad");
             System.out.println("3. Validador de número perfecto");
+            System.out.println("4. Validador de palíndromo");
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
             opcion = sc.nextInt();
+            sc.nextLine(); // limpiar buffer
 
             switch (opcion) {
                 case 1 -> ZodiacoApp.main(null);
                 case 2 -> ValidadorEdad.main(null);
                 case 3 -> ValidadorNumeroPerfecto.main(null);
+                case 4 -> ValidadorPalindromo.main(null);
                 case 0 -> System.out.println("Saliendo...");
                 default -> System.out.println("Opción inválida.");
             }
