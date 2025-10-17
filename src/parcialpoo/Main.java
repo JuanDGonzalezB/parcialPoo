aa0fbd1815f9a00f3aae05096e294390183d0a9b
Juan David Gonzalez Bermudez <juandgonzalezb@udistrital.edu.co>
2025-10-16 22:37:21 -0500

    Se actualiza el main con el ValidadorNumeroPerfecto

--- src/parcialpoo/Main.java
+++ src/parcialpoo/Main.java
@@ -12,6 +12,7 @@ public class Main {
             System.out.println("=== MENÚ PRINCIPAL ===");
             System.out.println("1. Zodiaco");
             System.out.println("2. Validador de edad");
+            System.out.println("3. Validador de número perfecto");
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
             opcion = sc.nextInt();
@@ -19,6 +20,7 @@ public class Main {
             switch (opcion) {
                 case 1 -> ZodiacoApp.main(null);
                 case 2 -> ValidadorEdad.main(null);
+                case 3 -> ValidadorNumeroPerfecto.main(null);
                 case 0 -> System.out.println("Saliendo...");
                 default -> System.out.println("Opción inválida.");
             }
