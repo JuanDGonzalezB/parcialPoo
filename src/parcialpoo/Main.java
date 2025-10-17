4263c7603ded33c4db893f0cbea0f40e1e0301b5
Juan David Gonzalez Bermudez <juandgonzalezb@udistrital.edu.co>
2025-10-16 22:22:52 -0500

    Se ajusta el menu con la clase ValidadorEdad

--- src/parcialpoo/Main.java
+++ src/parcialpoo/Main.java
@@ -11,12 +11,14 @@ public class Main {
         do {
             System.out.println("=== MENÚ PRINCIPAL ===");
             System.out.println("1. Zodiaco");
+            System.out.println("2. Validador de edad");
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
             opcion = sc.nextInt();
 
             switch (opcion) {
                 case 1 -> ZodiacoApp.main(null);
+                case 2 -> ValidadorEdad.main(null);
                 case 0 -> System.out.println("Saliendo...");
                 default -> System.out.println("Opción inválida.");
             }
